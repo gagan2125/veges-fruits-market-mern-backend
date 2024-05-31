39b196625cabc0e853aba9612cbf3908920c40f6
gagan2125 <gagansk2125@gmail.com>
2024-05-31 12:02:19 +0530

    sixth commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
