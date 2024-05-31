d198e12b9ea4674a9192c4d3a28f4ed71eb82830
gagan2125 <gagansk2125@gmail.com>
2024-05-31 12:51:57 +0530

    part10 commit

--- server.js
+++ server.js
@@ -8,25 +8,32 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Database Connected");
-  })
-  .catch((err) => console.log(err));
-
-app.get("/api/products", async (req, res) => {
+const startServer = async () => {
   try {
-    const allProducts = await Product.find();
-    res.json(allProducts);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error: "Internal Server Error",
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-  }
-});
+    console.log("Database Connected");
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    app.get("/api/products", async (req, res) => {
+      try {
+        const allProducts = await Product.find();
+        res.json(allProducts);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({
+          error: "Internal Server Error",
+        });
+      }
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to the database", err);
+  }
+};
+
+startServer();
