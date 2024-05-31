3234bd3daa1557de345ba2f12f85b9176f33d131
gagan2125 <gagansk2125@gmail.com>
2024-05-31 12:32:18 +0530

    part2 commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Product = require("./models/ProductSchems");
+const Product = require("./models/ProductSchema");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,105 +9,14 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Database Connected");
   })
   .catch((err) => console.log(err));
-
-// // initial data
-const seedDatabase = async () => {
-  try {
-    await Product.deleteMany(); // old data will be deleted once the server is restarted means
-    const products = [
-      {
-        name: "Apple",
-        type: "Fruit",
-        description: "Fresh and crispy",
-        price: 150,
-        image:
-          "https://media.geeksforgeeks.org/wp-content/uploads/20240104142542/apple.jpg",
-      },
-      {
-        name: "Banana",
-        type: "Fruit",
-        description: "Rich in potassium",
-        price: 75,
-        image:
-          "https://media.geeksforgeeks.org/wp-content/uploads/20240104142554/banana.jpg",
-      },
-      {
-        name: "Orange",
-        type: "Fruit",
-        description: "Packed with vitamin C",
-        price: 200,
-        image:
-          "https://media.geeksforgeeks.org/wp-content/uploads/20240104142641/orange.jpg",
-      },
-      {
-        name: "Carrot",
-        type: "Vegetable",
-        description: "Healthy and crunchy",
-        price: 100,
-        image:
-          "https://media.geeksforgeeks.org/wp-content/uploads/20240104142613/carrot.jpg",
-      },
-      {
-        name: "Broccoli",
-        type: "Vegetable",
-        description: "Nutrient-rich greens",
-        price: 175,
-        image:
-          "https://media.geeksforgeeks.org/wp-content/uploads/20240104142601/brocoli.jpg",
-      },
-      {
-        name: "Grapes",
-        type: "Fruit",
-        description: "Sweet and juicy",
-        price: 250,
-        image:
-          "https://media.geeksforgeeks.org/wp-content/uploads/20240104142629/grapes.jpg",
-      },
-      {
-        name: "Strawberry",
-        type: "Fruit",
-        description: "Delicious red berries",
-        price: 300,
-        image:
-          "https://media.geeksforgeeks.org/wp-content/uploads/20240104142657/strawberry.jpg",
-      },
-      {
-        name: "Lettuce",
-        type: "Vegetable",
-        description: "Crisp and fresh",
-        price: 120,
-        image:
-          "https://media.geeksforgeeks.org/wp-content/uploads/20240104142635/lettue.jpg",
-      },
-      {
-        name: "Tomato",
-        type: "Vegetable",
-        description: "Versatile and flavorful",
-        price: 180,
-        image:
-          "https://media.geeksforgeeks.org/wp-content/uploads/20240104142704/tomato.jpg",
-      },
-      {
-        name: "Cucumber",
-        type: "Vegetable",
-        description: "Cool and hydrating",
-        price: 130,
-        image:
-          "https://media.geeksforgeeks.org/wp-content/uploads/20240104142621/cocumber.jpg",
-      },
-    ];
-    await Product.insertMany(products); // data will be added once the server is restarted means
-    console.log("Database Seeded Successfully");
-  } catch (error) {
-    console.error("Error Seeding", error);
-  }
-};
-seedDatabase();
 
 app.get("/api/products", async (req, res) => {
   try {
@@ -122,5 +31,5 @@ app.get("/api/products", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running on port ${PORT}`);
 });
