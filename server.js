b8972d1110dfe64cd865417cc7372e1ffc088499
gagan2125 <gagansk2125@gmail.com>
2024-05-31 12:00:14 +0530

    fifth commit

--- server.js
+++ server.js
@@ -7,13 +7,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://crackmack16:YUgxJjwo8NXpscZl@cluster0.xyeoecj.mongodb.net/",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // MongoDB Schema
 const productSchema = new mongoose.Schema({
