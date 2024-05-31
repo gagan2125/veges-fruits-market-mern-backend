2ca6e4f0a0eb9c53414ac3e10973dfd858f1c60f
gagan2125 <gagansk2125@gmail.com>
2024-05-31 13:16:10 +0530

    part14 commit

--- server.js
+++ server.js
@@ -5,35 +5,40 @@ const Product = require("./models/ProductSchema");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-const startServer = async () => {
+// MongoDB Connection
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database Connected");
-
-    app.get("/api/products", async (req, res) => {
-      try {
-        const allProducts = await Product.find();
-        res.json(allProducts);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({
-          error: "Internal Server Error",
-        });
-      }
-    });
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
   } catch (err) {
     console.error("Failed to connect to the database", err);
+    process.exit(1);
   }
 };
+connectDB();
 
-startServer();
+// Routes
+app.get("/api/products", async (req, res) => {
+  try {
+    const allProducts = await Product.find();
+    res.json(allProducts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-// Export the app object
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
