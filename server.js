574296b706da87a9b69a7371eabf01d104a8065a
gagan2125 <gagansk2125@gmail.com>
2024-05-31 12:57:29 +0530

    part11 commit

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.use(express.json());
 
 const startServer = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Database Connected");
 
     app.get("/api/products", async (req, res) => {
